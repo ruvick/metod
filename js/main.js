24314448323802b14055fc455fe872d2091ba4e1
ruvick <rudikov.web@ya.ru>
2021-03-28 15:26:00 +0300

    Правки формы и стр.Отзывы

--- js/main.js
+++ js/main.js
@@ -837,6 +837,25 @@ $('.select-slider-big').slick({
 });
 
 
+// input file
+    let inputs = document.querySelectorAll('.input__file');
+    Array.prototype.forEach.call(inputs, function (input) {
+      let label = input.nextElementSibling,
+        labelVal = label.querySelector('.input__file-button-text').innerText;
+  
+      input.addEventListener('change', function (e) {
+        let countFiles = '';
+        if (this.files && this.files.length >= 1)
+          countFiles = this.files.length;
+  
+        if (countFiles)
+          label.querySelector('.input__file-button-text').innerText = 'Выбрано файлов: ' + countFiles;
+        else
+          label.querySelector('.input__file-button-text').innerText = labelVal;
+      });
+    });
+
+
 // Выбо колличества
 $('.minus').click(function () {
 	var $input = $(this).parent().find('input');
